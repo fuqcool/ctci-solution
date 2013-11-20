8f32907c69df828a0dde12a76045fbf6d0dba57f
fuqcool <fuqcool@gmail.com>
2013-11-20 16:08:37 -0500

    c -> javascript

---
+++ 01/replace_space.js
@@ -0,0 +1,21 @@
+/**
+ * GIVEN: a string
+ * RETURNS: replace the spaces with '%20'
+ */
+
+function replaceSpace(s) {
+  var result = '';
+
+  var i;
+  for (i = 0; i < s.length; i++) {
+    result += (s[i] === ' ') ? '%20' : s[i];
+  }
+
+  return result;
+}
+
+
+console.assert(replaceSpace('abcd') === 'abcd', 'should return the same string');
+console.assert(replaceSpace('a  b cd') === 'a%20%20b%20cd', 'should replace spaces');
+
+console.log('correct');
